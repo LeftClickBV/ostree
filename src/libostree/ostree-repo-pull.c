50f12e607aaca5e417f67925028449d4409db04f
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2017-02-11 01:26:12 +0100

    WIP: Add seperate proxy auth configuration
    Add configuration items for proxy auth seperate form proxy url as the
    url may be discovered via other means (e.g. PAC file). This also add
    support for enabling ntlm authentications
    
    TODO: Unbreak curl backend build

--- src/libostree/ostree-repo-pull.c
+++ src/libostree/ostree-repo-pull.c
@@ -3232,14 +3232,38 @@ _ostree_repo_remote_new_fetcher (OstreeRepo  *self,
 
   {
     g_autofree char *http_proxy = NULL;
+    g_autofree char *proxy_user = NULL;
+    g_autofree char *proxy_password = NULL;
+    gboolean proxy_ntlm = FALSE;
+
 
     if (!ostree_repo_get_remote_option (self, remote_name,
                                         "proxy", NULL,
                                         &http_proxy, error))
       goto out;
 
-    if (http_proxy != NULL && http_proxy[0] != '\0')
-      _ostree_fetcher_set_proxy (fetcher, http_proxy);
+    if (!ostree_repo_get_remote_option (self, remote_name,
+                                        "proxy-user", NULL,
+                                        &proxy_user, error))
+      goto out;
+
+    if (!ostree_repo_get_remote_option (self, remote_name,
+                                        "proxy-password", NULL,
+                                        &proxy_password, error))
+      goto out;
+
+    if (!ostree_repo_get_remote_boolean_option (self, remote_name,
+                                                "proxy-ntlm-auth", FALSE,
+                                                &proxy_ntlm, error))
+      goto out;
+
+
+    if ((http_proxy != NULL && http_proxy[0] != '\0') ||
+        (proxy_user != NULL && proxy_user[0] != '\0' &&
+         proxy_password != NULL && proxy_password[0] != '\0'))
+      _ostree_fetcher_set_proxy (fetcher, http_proxy,
+                                 proxy_user, proxy_password,
+                                 proxy_ntlm);
   }
 
   if (!_ostree_repo_remote_name_is_file (remote_name))
